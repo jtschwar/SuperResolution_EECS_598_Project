57ef14d936e8e727726f8406cd411c454247fa3a
Jonathan Schwartz <jonathanschwartz@Jonathans-MacBook-Pro-6.local>
2022-04-18 22:18:05 -0400

    Ready to start training

--- datasets.py
+++ datasets.py
@@ -34,3 +34,35 @@ class testDataset(Dataset):
     def __len__(self):
         with h5py.File(self.h5_file, 'r') as file:
             return len(file['lr'])
+
+
+# class H5_Dataset(Dataset):
+#     def __init__(self, hd5_file, database_name):
+#         self.database_name = database_name
+#         self.h5_file = h5py.File(hd5_file, 'r')
+#         super().__init__()
+#         self.index_dict = self.__register_indexes()
+ 
+#     def __register_indexes(self):
+#         """
+#         Returns a dictionary that assigns index
+#         to image location on the hdf5 file
+#         """
+#         index_dict = {}
+#         counter = 0
+#         for group, group_db in self.h5_file.items():
+#             for i in range(len(group_db[self.database_name])):
+#                 index_dict[counter] = {'group': group, 'index': i}
+#                 counter += 1
+#         return index_dict
+ 
+#     def __getitem__(self, index):
+#         item_info = self.index_dict[index]
+#         group = item_info['group']
+#         group_index = item_info['index']
+#         return {'group': group, 'image': self.h5_file[group][self.database_name][group_index]}
+#         # I havent written something about the labels,
+#         # because I don't know how your file is structured
+ 
+#     def __len__(self):
+#         return len(self.index_dict)
--- train.py
+++ train.py
@@ -3,10 +3,10 @@ from torch.utils.tensorboard import SummaryWriter
 from torch import nn
 import torch
 
-
 from datasets import trainDataset, testDataset
 from tqdm import tqdm
 import utils
+import time
 
 
 def train_model(inModel=None, train_file=None, eval_file=None,
@@ -14,56 +14,66 @@ def train_model(inModel=None, train_file=None, eval_file=None,
    
    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+   # torch.manual_seed(time.time())
+
    model = inModel.to(device)
    
    criterion = nn.MSELoss()
    optimizer = torch.optim.Adam([
       {'params': model.parameters(), 'lr': lr * 0.1}, ], lr = lr)
 
-   train_dataloader = DataLoader(dataset=trainDataset(train_file),
+   train_dataloader = DataLoader(dataset=trainDataset(train_file,'General100'),
                                  batch_size=batch_size,
                                  shuffle=True)
-   test_dataloader = DataLoader(dataset=testDataset(eval_file),
+   test_dataloader = DataLoader(dataset=testDataset(eval_file,'General100'),
                                 batch_size=1)
 
    # Create Trianing Process log file
-   writer = SummaryWriter()
+   # writer = SummaryWriter('logs')
 
+   best_epoch = 0
    best_psnr = 0.0
    for epoch in range(num_epochs):
       
       model.train()
       epoch_losses = utils.AverageMeter()
 
-      # with tqdm(total=(len(train)))
+      with tqdm(train_dataloader, unit='batch') as tepoch:
+         for input, target in tepoch:
+            tepoch.set_description(f"Epoch {epoch}")
 
-      for data in train_dataloader:
+            input = input.to(device); target = target.to(device)
+            optimizer.zero_grad()
 
-         inputs, labels = data
-         inputs = inputs.to(device); labels = labels.to(device)
-         
-         preds = model(inputs)
+            output = model(input)
 
-         loss = criterion(preds, labels)
+            loss = criterion(output, target)
+            epoch_losses.update(loss.time(),len(input))
 
-         epoch_losses.update(loss.time(),len(inputs))
+            loss.backward()
+            optimizer.step()
 
-         optimizer.zero_grad()
-         loss.backward()
-         optimizer.step()
-
+            tepoch.set_postfix(loss=loss.item())
+            
       model.eval()
       epoch_psnr = utils.AverageMeter()
 
       for data in test_dataloader:
 
-         inputs, labels = data
-         inputs = inputs.to(device); labels = labels.to(device)
+         inputs, targets = data
+         inputs = inputs.to(device); targets = targets.to(device)
 
-         with torch.no_grad(): pred = model(inputs).clamp(0.0,1.0)
+         with torch.no_grad(): preds = model(inputs).clamp(0.0,1.0)
 
-         epoch_psnr.update(utils.psnr(preds, labels), len(inputs))
+         epoch_psnr.update(utils.psnr(preds, targets), len(inputs))
+
+      if epoch_psnr.avg > best_psnr:
+         best_epoch = epoch 
+         best_psnr = epoch_psnr.avg
+
+         torch.save(model.state_dict(), 'model.pth')
 
       # epoch_losses ?
+   print('best epoch: {}, psnr: {:.2f}'.format(best_epoch,best_psnr))
 
       
